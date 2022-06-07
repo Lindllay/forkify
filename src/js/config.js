34d8b4478c6e3d14470ce7056f6136df69d0605e
Lindllay <maciejsemeniuk95@gmail.com>
2022-06-07 06:09:02 +0200

    config and helpers second commit

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes'; // Wszystko z dużej litery jest konwencją w przypadku tworzenia zmiennych w configu
+export const TIMEOUT_SEC = 10;
---
+++ src/js/helpers.js
@@ -0,0 +1,22 @@
+import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config.js';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
+
+export const getJSON = async function (url) {
+  try {
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]); // Po 0.5 sec, funkcja setTimeout jest rejected (rejected promise) i podaje error message. Następnie jeżeli jest to pierwszy rezultat w Promise.race spośród fetch(url) oraz timeout(0.5), to przerywa fetchowanie i rzuca błąd, który później jest ponownie rzucany w model.js
+    const { data } = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err; // re-throwing error. Muszę zrobić re-throw, żeby promise było rejected. Gdy mam jedną async funkcję wewnątrz drugiej, to chcąc łapać errory w tej zewnętrznej, muszę zrobić re-throwa tego errora w wewnętrznym
+  }
+};
