1a28dab72a213c044438220856b89b00cde0e232
Lindllay <maciejsemeniuk95@gmail.com>
2022-06-17 23:37:37 +0200

    project finished

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -9,6 +9,28 @@ const timeout = function (s) {
   });
 };
 
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const fetchPro = uploadData
+      ? fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json', // informacja dla API, że dane przesyłam w formie JSON, dzięki czemu może z powodzeniem je zaakceptować
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+    const res = await Promise.race([fetchPro, timeout(TIMEOUT_SEC)]); // Po 10 sec, funkcja setTimeout jest rejected (rejected promise) i podaje error message. Następnie jeżeli jest to pierwszy rezultat w Promise.race spośród fetch(url) oraz timeout(10), to przerywa fetchowanie i rzuca błąd, który później jest ponownie rzucany w model.js
+    const { data } = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    return data;
+  } catch (err) {
+    throw err; // re-throwing error. Muszę zrobić re-throw, żeby promise było rejected. Gdy mam jedną async funkcję wewnątrz drugiej, to chcąc łapać errory w tej zewnętrznej, muszę zrobić re-throwa tego errora w wewnętrznym
+  }
+};
+
+/*
 export const getJSON = async function (url) {
   try {
     const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]); // Po 10 sec, funkcja setTimeout jest rejected (rejected promise) i podaje error message. Następnie jeżeli jest to pierwszy rezultat w Promise.race spośród fetch(url) oraz timeout(10), to przerywa fetchowanie i rzuca błąd, który później jest ponownie rzucany w model.js
@@ -40,3 +62,4 @@ export const sendJSON = async function (url, uploadData) {
     throw err;
   }
 };
+ */
--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,7 @@
 import { async } from 'regenerator-runtime';
 import { API_URL, RES_PER_PAGE, KEY } from './config.js';
-import { getJSON, sendJSON } from './helpers.js';
+// import { getJSON, sendJSON } from './helpers.js';
+import { AJAX } from './helpers.js';
 
 export const state = {
   recipe: {},
@@ -30,7 +31,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
   try {
-    const data = await getJSON(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}`);
     state.recipe = createRecipeObject(data);
 
     if (state.bookmarks.some(bookmark => bookmark.id === id)) {
@@ -47,7 +48,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
-    const data = await getJSON(`${API_URL}?search=${query}`);
+    const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
 
     state.search.results = data.recipes.map(rec => {
       return {
@@ -55,6 +56,7 @@ export const loadSearchResults = async function (query) {
         title: rec.title,
         publisher: rec.publisher,
         image: rec.image_url,
+        ...(rec.key && { key: rec.key }), // and operator sprawi, że jeśli recipe.key będzie false (po prostu ten obiekt go nie będzie miał), to przerywa wykonywanie. a jeśli będzie true, to zwróci {key: recipe.key}, który zostanie zdestrukturyzowany. Jest to trick, który pozwala na dodawanie właściwości do obiektu po spełnieniu warunku
       };
     });
     state.search.page = 1; // resetuje page przy każdym załadowaniu nowej listy
@@ -118,7 +120,9 @@ export const uploadRecipe = async function (newRecipe) {
     const ingredients = Object.entries(newRecipe)
       .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
       .map(ing => {
-        const ingArr = ing[1].replaceAll(' ', '').split(',');
+        const ingArr = ing[1].split(',').map(el => el.trim());
+        console.log(ingArr);
+        // const ingArr = ing[1].replaceAll(' ', '').split(',');
         if (ingArr.length !== 3) {
           throw new Error(
             'Wrong ingredient format! Please use the correct format.'
@@ -139,7 +143,7 @@ export const uploadRecipe = async function (newRecipe) {
       ingredients,
     };
 
-    const data = await sendJSON(`${API_URL}?key=${KEY}`, recipe);
+    const data = await AJAX(`${API_URL}?key=${KEY}`, recipe);
     state.recipe = createRecipeObject(data);
     addBookmark(state.recipe);
   } catch (err) {
